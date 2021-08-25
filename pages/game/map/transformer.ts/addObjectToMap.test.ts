b92fca0525eac483a981fa0ab1d4a45b1dce6615
Tiago Rodrigues <tiagotk21@hotmail.com>
2021-08-25 20:34:28 -0300

    Defined exceptions and did some tests

--- pages/game/map/transformer.ts/addObjectToMap.test.ts
+++ pages/game/map/transformer.ts/addObjectToMap.test.ts
@@ -1,4 +1,4 @@
-import { expect } from 'chai';
+import { expect, fail } from 'chai';
 import addObjectToMap from './addObjectToMap';
 import basicMap from '../basicMap';
 
@@ -25,3 +25,57 @@ it('addObjectToMap empty map receive a object at position 2/1', () => {
     ]
   })
 })
+
+it('addObjectToMap add an object to a position that already have an object should fail', () => {
+  const newMapWithObject = addObjectToMap(basicMap, { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }, 2, 1)
+  expect(() => (
+    addObjectToMap(
+      newMapWithObject,
+      { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }
+      , 2, 1)
+    )).to.throw('Position is not empty');
+})
+
+it('addObjectToMap add an object to a position that already have an object should fail', () => {
+  expect(() => (
+    addObjectToMap(
+      basicMap,
+      { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }
+      , 0, -1)
+    )).to.throw('Position does´t exist');
+  expect(() => (
+    addObjectToMap(
+      basicMap,
+      { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }
+      , -1, 0)
+    )).to.throw('Position does´t exist');
+  expect(() => (
+    addObjectToMap(
+      basicMap,
+      { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }
+      , -1, -1)
+    )).to.throw('Position does´t exist');
+  expect(() => (
+    addObjectToMap(
+      basicMap,
+      { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }
+      , 0, 5)
+    )).to.throw('Position does´t exist');
+  expect(() => (
+    addObjectToMap(
+      basicMap,
+      { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }
+      , 5, 0)
+    )).to.throw('Position does´t exist');
+  expect(() => (
+    addObjectToMap(
+      basicMap,
+      { name: 'Player', group: 'ally', blockPass: true, keyReference: '1' }
+      , 5, 5)
+    )).to.throw('Position does´t exist');
+})
+
+
+// TODO
+// fail when try to add an object to a position that is out of the map
+// 
--- pages/game/map/transformer.ts/addObjectToMap.ts
+++ pages/game/map/transformer.ts/addObjectToMap.ts
@@ -4,6 +4,20 @@ const addObjectToMap = (map: GameMap, object: FloorObject, posX: number, posY: n
   const newMap = cloneDeep(map),
     newObject = cloneDeep(object);
   
+  if (
+    newMap.tiles[posX] === undefined
+    || newMap.tiles[posX][posY] === undefined
+  ) {
+    throw new Error('Position does´t exist');
+  }
+  
+  if (
+    newMap.tiles[posX][posY].objectIn !== null
+    && newMap.tiles[posX][posY].objectIn !== undefined
+  ) {
+    throw new Error('Position is not empty');
+  }
+
   newMap.tiles[posX][posY].objectIn = newObject;
 
   return newMap;
